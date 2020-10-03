c7190702fb2e610e360718fb3fc8ec0675291677
cami-jara <camilati.jarase@gmail.com>
2020-10-02 21:58:20 -0300

    Agregando imagenes para el sitio web

--- js/contacto.js
+++ js/contacto.js
@@ -1,45 +1,26 @@
 ("#enviar-btn").click(function(){
-    let mensajes = $("#mensajes");
-    let errores = [];
+    let nombre = $('#nombre-txt').val();
+    let apellido = $('#apellido-txt').val();
+    let correo = $('#correo-txt').val();
+    let telefono = $('#telefono-txt').val();
+    let mensaje = $('#mensaje-txt').val();
+  
+  
+    if(nombre == '' || apellido == '' || correo == '' || telefono == '' || mensaje == ''){
+  
+      Swal.fire({
+    title: 'Error',
+    text: 'Completa el formuralio',
+    icon: 'error',
+    confirmButtonText: 'Ok',
+      });
+    }else{
+      Swal.fire({
+    title: 'Gracias!',
+    text: 'Tú mensaje a sido enviado exitosamente',
+    icon: 'success',
+    confirmButtonText: 'Ok',
+    });
+  }
 
-    let nombre = $("#nombre-txt").val();
-    let apellido =$("#apellido-txt").val();
-    let correo = $("#correo-txt").val();
-    let telefono = $("#telefono-txt").val();
-    let mensaje = $("#mensaje-txt").val();
-    if(nombre == ''){
-       errores.push("Debe ingresar nombre");
-    }
-    if(apellido == ''){
-      errores.push("Debe ingresar apellido");
-    }
-    if(correo == ''){
-      errores.push("Debe ingresar correo");
-    }
-    if(telefono == ''){
-      errores.push("Debe ingresar telefono");
-    }
-    if(mensaje == ''){
-      errores.push("Debe ingresar mensaje");
-    }
-    if(errores.length == 0){
-    let contacto = {};
-        contacto.nombre = nombre;
-        contacto.apellido = apellido;
-        contacto.correo = correo;
-        contacto.telefono = telefono;
-        contacto.mensaje= mensaje;
-        Swal.fire({
-         title: "Formulario enviado",
-         text: "Mensaje fue enviado exitosamente.",
-         icon: "success",
-        });
-
-      }else {
-        Swal.fire({
-        title: "Formulario no enviado",
-        text: "Por favor complete todos los campos.",
-        icon: "error",
-       });
-      }
-  });
+});  
