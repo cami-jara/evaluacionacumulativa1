0cd44539a5acf033c8335dcd273b6a8dda8d8ff2
cami-jara <camilati.jarase@gmail.com>
2020-11-08 22:51:53 -0300

    Agregando api de facebook y google

--- js/google.js
+++ js/google.js
@@ -1,17 +1,17 @@
-{ 
-    client_id : '501804933970-spga90hlajn24tb33fkhng4nrg1p4dg9.apps.googleusercontent.com' } 
-    
-    
+
+  
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('ID: ' + profile.getId()); //¡No envíes a tu backend! Use un token de identificación en su lugar.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-     
-    function init() {
-        gapi.load('auth2', function() {
-          /* Ready. Make a call to gapi.auth2.init or some other API */
-        });
-      }
+    console.log('Email: ' + profile.getEmail()); // Esto es nulo si el alcance 'correo electrónico' no está presente.   
   }
+
+  function init() {
+    gapi.load('auth2', function() {
+      { client_id : 'CLIENT_ID.apps.googleusercontent.com' } 
+      /* Listo. Hacer una llamada a gapi.auth2.init o alguna otra API*/
+    });
+  }
+  
